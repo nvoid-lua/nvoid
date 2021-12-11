a3221e1fac4db22443cfa8d5573ddeda02f51338
ysfgrg <ysfgrgO7@gmail.com>
2021-12-11 17:01:50 +0200

    nvoid

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,16 +1,4 @@
 local g = vim.g
-
-g.nvim_tree_quit_on_open = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_git_hl = 1 
-g.nvim_tree_add_trailing = 1
-g.nvim_tree_group_empty = 1
-g.nvim_tree_disable_window_picker = 1
-g.nvim_tree_icon_padding = ' '
-g.nvim_tree_symlink_arrow = ' >> '
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_create_in_closed_folder = 0
-g.nvim_tree_refresh_wait = 500
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 require'nvim-tree'.setup {
