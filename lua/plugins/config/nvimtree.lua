423f493c8d5b83d82cdf79cfb02ba7bdb575edcd
ysfgrg <ysfgrgO7@gmail.com>
2021-12-11 16:19:22 +0200

    nvoid

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,12 +1,24 @@
 local g = vim.g
+
+g.nvim_tree_quit_on_open = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_git_hl = 1 
+g.nvim_tree_add_trailing = 1
+g.nvim_tree_group_empty = 1
+g.nvim_tree_disable_window_picker = 1
+g.nvim_tree_icon_padding = ' '
+g.nvim_tree_symlink_arrow = ' >> '
+g.nvim_tree_respect_buf_cwd = 1
+g.nvim_tree_create_in_closed_folder = 0
+g.nvim_tree_refresh_wait = 500
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 require'nvim-tree'.setup {
    disable_netrw = true,
    hijack_netrw = true,
    ignore_ft_on_setup = { "dashboard" },
-   auto_close = false,
-   open_on_tab = false,
+   auto_close = true,
+   open_on_tab = true,
    hijack_cursor = true,
    update_cwd = true,
   update_focused_file = {
@@ -29,7 +41,7 @@ require'nvim-tree'.setup {
     side = 'left',
     auto_resize = true,
     mappings = {
-      custom_only = false,
+      custom_only = true,
       list = {}
     }
   }
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -57,7 +57,7 @@ end
 time([[Luarocks path setup]], false)
 time([[try_loadstring definition]], true)
 local function try_loadstring(s, component, name)
-  local success, result = pcall(loadstring(s))
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
   if not success then
     vim.schedule(function()
       vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
