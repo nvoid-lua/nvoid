255e19abfe2e1a1f36d369256de90d2cdade19f0
ysfgrg <ysfgrgO7@gmail.com>
2021-12-12 14:30:02 +0200

    New plugins sys

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 local present, packer = pcall(require, "plugins.packerInit")
+require("plugins/plugConfig")
 if not present then
    return false
 end
@@ -13,43 +14,35 @@ return packer.startup(function()
 -- Icons
    use {
        "kyazdani42/nvim-web-devicons",
-      config = require("plugins/config/icons-config")
    }
 -- Feline
    use {
        "famiu/feline.nvim",
-       config = require("plugins/config/statusline")
    }
 -- Bufferline
    use {
        "akinsho/bufferline.nvim",
-       config = require("plugins/config/bufferline")
    }
    use { "Asheq/close-buffers.vim" }
 -- Indent Line
    use {
        "lukas-reineke/indent-blankline.nvim",
-       config = require("plugins/config/indentline")
    }
 -- Colorizer
    use {
        "norcalli/nvim-colorizer.lua",
-       config = require("plugins/config/colorizer")
    }
 -- Tree Sitter
    use {
        "nvim-treesitter/nvim-treesitter",
-       config = require("plugins/config/treesitter")
    }
 -- Git Sign
    use {
        "lewis6991/gitsigns.nvim",
-       config = require("plugins/config/git")
    }
 -- LSP
    use {
        "neovim/nvim-lspconfig",
-       config = require("plugins/config/lsp")
    }
 
    use { "glepnir/lspsaga.nvim" }
@@ -59,7 +52,6 @@ return packer.startup(function()
 -- CMP
    use {
        "hrsh7th/nvim-cmp",
-       config = require("plugins/config/cmp")
    }
    use { "L3MON4D3/LuaSnip" }
    use { "saadparwaiz1/cmp_luasnip" }
@@ -70,24 +62,20 @@ return packer.startup(function()
 -- Auto Pairs
    use {
        "windwp/nvim-autopairs",
-       config = require("plugins/config/autopairs")
    }
 -- Dashboard
    use {
        "glepnir/dashboard-nvim",
-       config = require("plugins/config/dashboard")
    }
 -- Comment
    use { "winston0410/commented.nvim" }
 -- Nvim Tree
    use {
        "kyazdani42/nvim-tree.lua",
-       config = require("plugins/config/nvimtree")
    }
 -- Telescope
    use {
        "nvim-telescope/telescope.nvim",
-       config = require("plugins/config/telescope")
    }
 -- Colorschemes
    use { "Lunarvim/Onedarker" }
@@ -99,7 +87,6 @@ return packer.startup(function()
 -- Other
    use {
        "akinsho/toggleterm.nvim",
-       config = require("plugins/config/term")
    }
    use { "nvim-lua/plenary.nvim" }
    use { "ryanoasis/vim-devicons" }
@@ -107,6 +94,5 @@ return packer.startup(function()
    use { "andymass/vim-matchup" }
    use {
        "folke/which-key.nvim",
-       config = require("plugins/config/which-key")
    }
 end)
