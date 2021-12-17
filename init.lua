09424ccde9cb5be19b337167f900056e1bb05197
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-17 14:25:03 +0200

    init

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
-require("plugins")
-require("plugins/plugConfig")
-require("core")
-require("colors")
+if vim.fn.has('nvim-0.5') == 0 then
+  error('Need NVIM 0.5 in order to run Nvoid!!')
+end
+
+local modules = {
+   "plugins",
+   "plugins/plugConfig",
+   "core",
+   "colors"
+}
+
+for _, module in ipairs(modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
+   end
+end
