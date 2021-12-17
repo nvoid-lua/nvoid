2dd6d6a8e947e76769be20002b3e81a12aa39281
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-17 21:39:42 +0200

    statusline

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 if vim.fn.has('nvim-0.5') == 0 then
   error('Need NVIM 0.5 in order to run Nvoid!!')
 end
-require("nvoid")
+
+local ok, err = pcall(require, 'nvoid')
+
+if not ok then
+  error(('Error loading core...\n\n%s'):format(err))
+end
