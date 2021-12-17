9fb9ca3e33c8e6424ec9a6d9176a0ebf1f363fe8
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-17 15:39:54 +0200

    Lsp install

--- .github/NVOID-Install.sh
+++ .github/NVOID-Install.sh
@@ -15,4 +15,11 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
 nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+cd ~/.local/share/nvim/
+mkdir -p ~/.local/share/nvim/lsp_servers/
+mkdir -p ~/.local/share/nvim/lsp_servers/sumneko_lua/
+cd ~/.local/share/nvim/lsp_servers/sumneko_lua/
+echo "## Lua LSP ##"
+curl -fsSL "https://github.com/sumneko/vscode-lua/releases/download/v2.5.3/lua-2.5.3.vsix" > sumneko_lua.vsix
+unzip sumneko_lua.vsix
 nvim
