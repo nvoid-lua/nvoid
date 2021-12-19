67d62af4dd6d93801857098b88449f4beadd1fea
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-19 17:08:20 +0200

    cool stuff

--- lua/nvoid/plugins/init.lua
+++ lua/nvoid/plugins/init.lua
@@ -7,7 +7,7 @@ local commit = {
 vim.cmd [[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    autocmd BufWritePost init.lua source <afile> | PackerCompile
   augroup end
 ]]
 
