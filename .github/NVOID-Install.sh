c055db20b091dd3b6fb44debf644c420ded898b5
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-19 12:55:53 +0200

    cool stuff

--- .github/NVOID-Install.sh
+++ .github/NVOID-Install.sh
@@ -11,8 +11,8 @@ mv ~/.config/nvim ~/.config/NV.bc
 mkdir -p ~/.local/share/nvim/
 sudo npm i -g neovim
 pip install pynvim
-# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- # ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
 mkdir -p ~/.local/share/nvim/lsp_servers/
 mkdir -p ~/.local/share/nvim/lsp_servers/sumneko_lua/
