9994be4b408f7085621fe2cfee205faa8b3766c8
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-19 12:54:18 +0200

    cool stuff

--- lua/nvoid/plugins/init.lua
+++ lua/nvoid/plugins/init.lua
@@ -2,47 +2,22 @@ local commit = {
   cmp_path = "d83839ae510d18530c6d36b662a9e806d4dceb73",
   nvim_lsp_installer = "d7b10b13d72d4bf8f7b34779ddc3514bcc26b0f2",
 }
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
-end
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
+local present, packer = pcall(require, "nvoid.plugins.packerInit")
+
+if not present then
+   return false
 end
 
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+local use = packer.use
 
-return packer.startup(function(use)
+return packer.startup(function()
 -- Packer
-    use { "wbthomason/packer.nvim" }
+   use { "wbthomason/packer.nvim" }
 -- Icons
    use { "kyazdani42/nvim-web-devicons" }
 -- Feline
-   use { "famiu/feline.nvim" }
+   use { "famiu/feline.nvim", config = ("nvoid.plugins.config.statusline") }
 -- Bufferline
    use { "akinsho/bufferline.nvim" }
 -- Buffer Close
