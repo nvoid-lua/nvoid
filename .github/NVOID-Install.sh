d4bbc19ca7f5763cb103bba024fcba9a1704551a
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-21 21:44:35 +0200

    NVOID-Install.sh

--- .github/NVOID-Install.sh
+++ .github/NVOID-Install.sh
@@ -12,5 +12,7 @@ mv ~/.config/nvim ~/.config/NV.bc
 mkdir -p ~/.local/share/nvim/
 sudo npm i -g neovim
 pip install pynvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim
