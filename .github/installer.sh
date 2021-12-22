9f3be86de3ef614305099dd0557700d450caa2bb
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-22 22:01:06 +0200

    NVOID-Install.sh

---
+++ .github/installer.sh
@@ -0,0 +1,49 @@
+#!/bin/env bash
+
+function msg() {
+  local text="$1"
+  local div_width="80"
+  printf "%${div_width}s\n" ' ' | tr ' ' -
+  printf "%s\n" "$text"
+}
+
+function install_deps() {
+  if command -v pacman &> /dev/null
+  then
+	  sudo pacman -S --noconfirm --needed nodejs xclip fd ripgrep
+  elif command -v xbps-install &> /dev/null
+  then
+  	  sudo xbps-install -Syu nodejs fd xclip ripgrep
+  fi
+}
+ function install_lua_lsp() {
+   nvim -c 'LspInstall sumneko_lua'
+ }
+
+function install_bash_lsp() {
+  nvim -c 'LspInstall bashls'
+}
+
+msg "Would you like to install some dependencies?"
+read -p "[y]es or [n]o (default: no) : " -r answer
+[ "$answer" != "${answer#[Yy]}" ] && install_deps
+
+if [ -d "$HOME/.config/nvim/" ]; 
+then 
+  mv ~/.config/nvim/ ~/.config/NV_BC
+fi
+
+if [ -d "$HOME/.local/share/nvim/" ]; 
+then 
+  mv ~/.local/share/nvim/ ~/.local/share/NV_BC
+fi
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
+nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+msg "Would you like to install luals?"
+read -p "[y]es or [n]o (default: no) : " -r answer
+[ "$answer" != "${answer#[Yy]}" ] && install_lua_lsp 
