4a3d41fd2764ad0b8a63bac05a4935bb672eff14
ysfgrgO7 <ysfgrg2007@gmail.com>
2021-12-26 20:38:51 +0200

    nvoid

--- lua/nvoid/plugins/config/lualine/init.lua
+++ lua/nvoid/plugins/config/lualine/init.lua
@@ -1,9 +1,7 @@
--- local config = require("nvoid.core.utils").load_config()
+local config = require("nvoid.core.utils").load_config()
 
--- if config.ui.statusline == 'nvoid' then
-    -- require("nvoid.plugins.config.lualine.nvoid")
--- elseif config.ui.statusline == 'vscode' then
-    -- require("nvoid.plugins.config.lualine.vscode")
--- end
-
-require("nvoid.plugins.config.lualine.nv")
+if config.ui.statusline == 'nvoid' then
+ require("nvoid.plugins.config.lualine.nvoid")
+elseif config.ui.statusline == 'vscode' then
+ require("nvoid.plugins.config.lualine.vscode")
+end
