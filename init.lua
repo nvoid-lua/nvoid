cb66fccea7226568b7598b73f84e23e35bebb652
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-01-01 12:21:48 +0200

    nvoid

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
-if vim.fn.has('nvim-0.5') == 0 then
-  error('Need NVIM 0.5 in order to run Nvoid!!')
+if vim.fn.has('nvim-0.6') == 0 then
+  error('Need NVIM 0.6 in order to run Cosmic!!')
 end
 
 local ok, err = pcall(require, 'nvoid')
-pcall(require, "nvoidrc")
 
 if not ok then
-  return
+  error(('Error loading core...\n\n%s'):format(err))
 end
