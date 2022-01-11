76e9016b3525f3f70c8059827760b37728d78b12
ysf <ysfgrg2007@gmail.com>
2022-01-11 17:17:36 +0200

    Alpha

--- lua/nvoid/plugins/config/alpha.lua
+++ lua/nvoid/plugins/config/alpha.lua
@@ -1,112 +1,63 @@
-local present, alpha = pcall(require, "alpha")
-if not present then
-   return
-end
+-- Local Variables
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+local function button(shortcut, text, cmd)
+    local sc = shortcut:gsub('%s', ''):gsub('LDR', '<space>')
 
-local header = {
-    type = "text",
-    val = {
-[[]],
-[[ ███╗   ██╗██╗   ██╗ ██████╗ ██╗██████╗  ]],
-[[ ████╗  ██║██║   ██║██╔═══██╗██║██╔══██╗ ]],
-[[ ██╔██╗ ██║██║   ██║██║   ██║██║██║  ██║ ]],
-[[ ██║╚██╗██║╚██╗ ██╔╝██║   ██║██║██║  ██║ ]],
-[[ ██║ ╚████║ ╚████╔╝ ╚██████╔╝██║██████╔╝ ]],
-[[ ╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═╝╚═════╝  ]],
-[[]],
-    },
-    opts = {
-        position = "center",
-        hl = "AlphaHeader"
-    }
-}
-
-local handle = io.popen('fd -d 2 . $HOME"/.local/share/nvim/site/pack/packer" | head -n -2 | wc -l | tr -d "\n" ')
-local plugins = handle:read("*a")
-handle:close()
-
-local plugin_count = {
-    type = "text",
-    val = "Nvoid Loaded " .. plugins .. " plugins  ",
-    opts = {
-        position = "center",
-        hl = "AlphaHeader",
-    }
-}
-
-local footer = {
-    type = "text",
-    val = "爵 nvoid.org",
-    opts = {
-        position = "center",
-        hl = "AlphaFooter",
-    }
-}
-
-local function button(sc, txt, keybind)
-    local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
-
-    local opts = {
-        position = "center",
-        text = txt,
-        shortcut = sc,
+    local button_opts = {
+        position = 'center',
+        text = text,
+        shortcut = shortcut,
         cursor = 5,
         width = 20,
-        align_shortcut = "right",
-        hl_shortcut = "AlphaButtons",
-        hl = "AlphaButtons",
+        align_shortcut = 'right',
+        hl_shortcut = 'AlphaBody',
+        hl = 'AlphaBody',
     }
-    if keybind then
-        opts.keymap = {"n", sc_, keybind, {noremap = true, silent = true}}
+    if cmd then
+        button_opts.keymap = { 'n', sc, cmd, { noremap = true, silent = true } }
     end
 
+    -- Returnd default button data
     return {
-        type = "button",
-        val = txt,
+        type = 'button',
+        val = text,
         on_press = function()
-            local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
-            vim.api.nvim_feedkeys(key, "normal", false)
+            local key = vim.api.nvim_replace_termcodes(sc, true, false, true)
+            vim.api.nvim_feedkeys(key, 'normal', false)
         end,
-        opts = opts,
+        opts = button_opts,
     }
 end
+-- local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+local nvoid_site = "爵nvoid.org"
 
-local buttons = {
-    type = "group",
-    val = {
-    button( "n", "  > New file" ,   ":enew<CR>"),
-    button( "f", "  > Find file",   ":Telescope find_files<CR>"),
-    button( "o", "  > Recent"   ,   ":Telescope oldfiles<CR>"),
-    button( "e", "  > Edit Confg" , ":e ~/.config/nvim/lua/custom/nvoidrc.lua<CR>"),
+-- Set header
+dashboard.section.header.val = {
+  "                                         ",
+  " ███╗   ██╗██╗   ██╗ ██████╗ ██╗██████╗  ",
+  " ████╗  ██║██║   ██║██╔═══██╗██║██╔══██╗ ",
+  " ██╔██╗ ██║██║   ██║██║   ██║██║██║  ██║ ",
+  " ██║╚██╗██║╚██╗ ██╔╝██║   ██║██║██║  ██║ ",
+  " ██║ ╚████║ ╚████╔╝ ╚██████╔╝██║██████╔╝ ",
+  " ╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═╝╚═════╝  ",
+  "                                         ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    button( "n", "  > New file" , ":enew<CR>"),
+    button( "f", "  > Find file", ":Telescope find_files<CR>"),
+    button( "o", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    button( "e", "  > Edit Confg" , ":e ~/.config/nvim/lua/nvoidrc.lua<CR>"),
     button( "c", "  > ColorScheme", ":Telescope colorscheme<CR>"),
-    button( "q", "  > Quit NVIM",   ":qa<CR>"),
-    },
-    opts = {
-        spacing = 1,
-    }
+    button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
 
-local section = {
-    header = header,
-    buttons = buttons,
-    plugin_count = plugin_count,
-    footer = footer
+-- Set Footer
+dashboard.section.footer.val = {
+  nvoid_site,
 }
 
-local opts = {
-    layout = {
-        {type = "padding", val = 2},
-        section.header,
-        {type = "padding", val = 1},
-        section.heading,
-        section.plugin_count,
-        {type = "padding", val = 1},
-        section.buttons,
-        {type = "padding", val = 1},
-        section.footer,
-    },
-    opts = {
-        margin = 5
-    },
-}
-alpha.setup(opts)
+-- Important
+alpha.setup(dashboard.opts)
