914098d1dd25cb546621c72048127e143fa1b679
ysf <ysfgrg2007@gmail.com>
2022-01-11 18:11:28 +0200

    map

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
-if vim.fn.has('nvim-0.6') == 0 then
-  error('Need NVIM 0.6 in order to run Cosmic!!')
+if vim.fn.has("nvim-0.6") == 0 then
+	error("Need NVIM 0.6 in order to run Cosmic!!")
 end
 
 pcall(require, "custom.nvoidrc")
 
-for _, module in ipairs({"nvoid"}) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
+for _, module in ipairs({ "nvoid" }) do
+	local ok, err = pcall(require, module)
+	if not ok then
+		error("Error loading " .. module .. "\n\n" .. err)
+	end
 end
