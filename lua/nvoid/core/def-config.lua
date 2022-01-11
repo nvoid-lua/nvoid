0ff1b26ea54b40039d4ef5d48d95dd273eec4e8e
ysf <ysfgrg2007@gmail.com>
2022-01-11 18:26:45 +0200

    adding null ls

--- lua/nvoid/core/def-config.lua
+++ lua/nvoid/core/def-config.lua
@@ -2,43 +2,43 @@ local M = {}
 
 -- UI
 M.ui = {
-  theme = 'onedarker',
-  transparency = false,
-  statusline = 'nvoid',
+	theme = "onedarker",
+	transparency = false,
+	statusline = "nvoid",
 }
 
 -- OPT
 M.options = {
-  clipboard = 'unnamedplus',
-  cmdheight = 1,
-  mouse = 'a',
-  mapleader = ' ',
-  wrap = false,
-  number = true,
-  relative_number = false,
-  number_width = 6,
-  cursor_line = true,
-  hidden = true,
-  expand_tab = true,
-  ignore_case = true,
-  smart_case = true,
-  smart_indent = true,
-  swap_file = false,
-  backup = false,
-  show_mode = false,
+	clipboard = "unnamedplus",
+	cmdheight = 1,
+	mouse = "a",
+	mapleader = " ",
+	wrap = false,
+	number = true,
+	relative_number = false,
+	number_width = 6,
+	cursor_line = true,
+	hidden = true,
+	expand_tab = true,
+	ignore_case = true,
+	smart_case = true,
+	smart_indent = true,
+	swap_file = false,
+	backup = false,
+	show_mode = false,
 }
 
 -- Add Treesitter langs
 M.ts_add = {
-  "bash",
-  "lua",
-  "c",
-  "cpp",
-  "css",
-  "javascript",
-  "json",
-  "yaml",
-  "python",
+	"bash",
+	"lua",
+	"c",
+	"cpp",
+	"css",
+	"javascript",
+	"json",
+	"yaml",
+	"python",
 }
 
 return M
