832b0e756570069b465f3f78abdf30f1e4e9b16f
ysf <ysfgrg2007@gmail.com>
2022-01-12 21:02:28 +0200

    adding nvchad theme

--- lua/nvoid/init.lua
+++ lua/nvoid/init.lua
@@ -1,13 +1,13 @@
 local modules = {
-   "nvoid.plugins",
-   "nvoid.core",
-   "nvoid.colors",
-   "nvoid.core.map"
+	"nvoid.plugins",
+	"nvoid.core",
+	"nvoid.colors",
+	"nvoid.core.map",
 }
 
 for _, module in ipairs(modules) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
+	local ok, err = pcall(require, module)
+	if not ok then
+		error("Error loading " .. module .. "\n\n" .. err)
+	end
 end
