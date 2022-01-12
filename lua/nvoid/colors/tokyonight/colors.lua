9362468f8601dd52acef5d56d16881c98065a845
ysf <ysfgrg2007@gmail.com>
2022-01-12 21:03:41 +0200

    formattings

--- lua/nvoid/colors/tokyonight/colors.lua
+++ lua/nvoid/colors/tokyonight/colors.lua
@@ -2,42 +2,42 @@ local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
 local colors = {
-   white = "#c0caf5",
-   darker_black = "#151621",
-   black = "#1A1B26", --  nvim bg
-   black2 = "#1f2335",
-   bg = "#1f2335",
-   one_bg = "#242530",
-   one_bg2 = "#292a35",
-   one_bg3 = "#353b45",
-   grey = "#40486a",
-   grey_fg = "#4a5274",
-   grey_fg2 = "#4f5779",
-   light_grey = "#545c7e",
-   red = "#f7768e",
-   baby_pink = "#DE8C92",
-   pink = "#ff75a0",
-   line = "#242530", -- for lines like vertsplit
-   green = "#9ece6a",
-   vibrant_green = "#73daca",
-   nord_blue = "#80a8fd",
-   blue = "#7aa2f7",
-   blue_dark = "#7aa2f7",
-   yellow = "#e7c787",
-   sun = "#EBCB8B",
-   purple = "#bb9af7",
-   dark_purple = "#9d7cd8",
-   teal = "#0db9d7",
-   orange = "#ff9e64",
-   cyan = "#7dcfff",
-   statusline_bg = "#1d1e29",
-   lightbg = "#252631",
-   lightbg2 = "#22232e",
-   pmenu_bg = "#7aa2f7",
-   folder_bg = "#7aa2f7",
+	white = "#c0caf5",
+	darker_black = "#151621",
+	black = "#1A1B26", --  nvim bg
+	black2 = "#1f2335",
+	bg = "#1f2335",
+	one_bg = "#242530",
+	one_bg2 = "#292a35",
+	one_bg3 = "#353b45",
+	grey = "#40486a",
+	grey_fg = "#4a5274",
+	grey_fg2 = "#4f5779",
+	light_grey = "#545c7e",
+	red = "#f7768e",
+	baby_pink = "#DE8C92",
+	pink = "#ff75a0",
+	line = "#242530", -- for lines like vertsplit
+	green = "#9ece6a",
+	vibrant_green = "#73daca",
+	nord_blue = "#80a8fd",
+	blue = "#7aa2f7",
+	blue_dark = "#7aa2f7",
+	yellow = "#e7c787",
+	sun = "#EBCB8B",
+	purple = "#bb9af7",
+	dark_purple = "#9d7cd8",
+	teal = "#0db9d7",
+	orange = "#ff9e64",
+	cyan = "#7dcfff",
+	statusline_bg = "#1d1e29",
+	lightbg = "#252631",
+	lightbg2 = "#22232e",
+	pmenu_bg = "#7aa2f7",
+	folder_bg = "#7aa2f7",
 }
 
 return colors
