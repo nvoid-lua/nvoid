ef3ee066e41ca488ea0f15c95c059d49ffbca4f9
ysf <ysfgrg2007@gmail.com>
2022-01-13 19:33:06 +0200

    which-key

--- .github/installer.sh
+++ .github/installer.sh
@@ -1,15 +1,5 @@
 #!/bin/env bash
 
-if [ -d "$HOME/.config/nvim/" ]; 
-then 
-  mv ~/.config/nvim/ ~/.config/NV_BC
-fi
-
-if [ -d "$HOME/.local/share/nvim/" ]; 
-then 
-  mv ~/.local/share/nvim/ ~/.local/share/NV_BC
-fi
-
 warnnvim() {
   echo "Please install neovim"
   exit
@@ -35,14 +25,15 @@ which git >/dev/null && echo "Git is installed" || warngit
 which node >/dev/null && echo "Node is installed" || warnnode
 which pip >/dev/null && echo "Node is installed" || warnpip
 
+git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
 
 npm install -g -f fd-find
 npm install -g -f neovim
-npm install -g -h tree-sitter-cli
+npm install -g -f tree-sitter-cli
 python3 -m pip install --user pynvim
 
 nvim -c 'PackerSync'
