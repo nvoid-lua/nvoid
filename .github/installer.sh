7b8fc800e181b3f16778b9b294e5f25c81b08dc1
ysf <ysfgrg2007@gmail.com>
2022-01-15 19:13:54 +0200

    which-key

--- .github/installer.sh
+++ .github/installer.sh
@@ -1,39 +1,158 @@
 #!/bin/env bash
 
+## Checking neovim
+pkg_nvim_installed() {
+  if command -v pacman &> /dev/null
+  then
+    echo "sudo pacman -Syu --needed neovim"
+  elif command -v xbps-install &> /dev/null
+  then
+    echo "sudo xbps-install -Syu neovim"
+  elif command -v dnf &> /dev/null
+  then
+   echo "sudo dnf install neovim"
+  fi
+}
 warnnvim() {
-  echo "Please install neovim"
+  echo "### Neovim is not installed try:"
+  echo $(pkg_nvim_installed)
   exit
 }
-
-warnnode() {
-  echo "Please install node"
-  exit
+function check_neovim_min_version() {
+  local verify_version_cmd='if !has("nvim-0.6.0") | cquit | else | quit | endif'
+  if ! nvim --headless -u NONE -c "$verify_version_cmd"; then
+    echo "[ERROR]: Nvoid requires at least Neovim v0.6.0 or higher"
+    exit 1
+  fi
 }
+check_neovim_min_version
 
-warnpip() {
-  echo "Please install pip"
-  exit
+which nvim >/dev/null && echo "Neovim 0.6.0 is installed" || warnnvim
+
+## Checking git
+pkg_git_installed() {
+  if command -v pacman &> /dev/null
+  then
+    echo "sudo pacman -Syu --needed git"
+  elif command -v xbps-install &> /dev/null
+  then
+    echo "sudo xbps-install -Syu git"
+  elif command -v dnf &> /dev/null
+  then
+   echo "sudo dnf install git"
+  fi
 }
-
 warngit() {
-  echo "Please install git"
+  echo "### Git is not installed try:"
+  echo $(pkg_git_installed)
   exit
 }
-
-which nvim >/dev/null && echo "Neovim is installed" || warnnvim
 which git >/dev/null && echo "Git is installed" || warngit
-which node >/dev/null && echo "Node is installed" || warnnode
-which pip >/dev/null && echo "Node is installed" || warnpip
 
-git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
+## Deps
+  function install_deps () {
+### MSG Function 
+  function msg() {
+    local text="$1"
+    local div_width="80"
+    printf "%${div_width}s\n" ' ' | tr ' ' -
+    printf "%s\n" "$text"
+  }
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+### npm deps START
+  function install_nodejs_deps() {
+    pkg_nodejs_install() {
+      if command -v pacman &> /dev/null
+      then
+        echo "sudo pacman -Syu --needed nodejs npm"
+      elif command -v xbps-install &> /dev/null
+      then
+        echo "sudo xbps-install -Syu nodejs"
+      elif command -v dnf &> /dev/null
+      then
+       echo "sudo dnf install nodejs npm"
+      fi
+    }
+    warnnode() {
+      echo "### nodejs or npm are not installed try:"
+      echo $(pkg_nodejs_install)
+      exit
+    }
+    which node npm && echo "Node and npm are installed" || warnnode
+    declare -a __npm_deps=(
+      "fd-find"
+      "neovim"
+      "tree-sitter-cli"
+    )
+    for deps in "${__npm_deps[@]}"; do
+      sudo npm i -g "$deps" --force
+    done
+  }
 
+  msg "Would you like to install nodejs dependencies?"
+  read -p "[y]es or [n]o (default: no) : " -r answer
+  [ "$answer" != "${answer#[Yy]}" ] && install_nodejs_deps
+### npm deps END
 
-sudo npm install -g -f fd-find
-sudo npm install -g -f neovim
-sudo npm install -g -f tree-sitter-cli
-sudo python3 -m pip install --user pynvim
+### pip deps START
+  function install_pip_deps() {
+    pkg_pip_install() {
+      if command -v pacman &> /dev/null
+      then
+        echo "sudo pacman -Syu --needed python-pip"
+      elif command -v xbps-install &> /dev/null
+      then
+        echo "sudo xbps-install -Syu python3-pip"
+      elif command -v dnf &> /dev/null
+      then
+       echo "sudo dnf install python-pip"
+      fi
+    }
+    warnpip() {
+      echo "### pip is not installed try:"
+      echo $(pkg_pip_install)
+      exit
+    }
+    which pip && echo "pip is installed" || warnpip
+    declare -a __pip_deps=(
+      "pynvim"
+    )
+    for deps in "${__pip_deps[@]}"; do
+      pip install --force-reinstall "$deps"
+    done
+  }
 
-nvim -c 'PackerSync'
+  msg "Would you like to install Python dependencies?"
+  read -p "[y]es or [n]o (default: no) : " -r answer
+  [ "$answer" != "${answer#[Yy]}" ] && install_pip_deps
+### pip deps END
+}
+
+clone_repo() {
+  git clone https://github.com/ysfgrgO7/nvoid.git ~/.config/nvim
+}
+
+clone_packer() {
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+}
+
+open_nvim() {
+  nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+  nvim
+}
+
+copy_old_config() {
+  if [ -d "$HOME/.local/share/nvim" ]; then 
+    mv ~/.local/share/nvim ~/.local/share/NV.old
+  fi
+  if [ -d "$HOME/.config/nvim" ]; then 
+    mv ~/.config/nvim ~/.config/NV.old
+  fi
+}
+
+install_deps
+copy_old_config
+clone_repo
+clone_packer
+open_nvim
