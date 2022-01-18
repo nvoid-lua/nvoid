a0b18b40f985e2957531d562b735b9f60819ada2
ysf <ysfgrg2007@gmail.com>
2022-01-18 12:36:23 +0200

    null-ls

--- lua/nvoid/plugins/config/lsp/settings/null-ls.lua
+++ lua/nvoid/plugins/config/lsp/settings/null-ls.lua
@@ -7,7 +7,7 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
+	debug = true,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
@@ -17,10 +17,10 @@ null_ls.setup({
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
 			vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
+              augroup LspFormatting
+                  autocmd! * <buffer>
+                  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+              augroup END
             ]])
 		end
 	end,
