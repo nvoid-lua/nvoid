da033bae0ade12571f1bab97b76e856a7152d74c
ysf grg <ysfgrg2007@gmail.com>
2022-01-21 17:44:32 +0200

    cleaning

--- lua/nvoid/plugins/config/lualine/init.lua
+++ lua/nvoid/plugins/config/lualine/init.lua
@@ -3,7 +3,7 @@ local config = require("nvoid.core.utils").load_config()
 if config.ui.statusline == "nvoid" then
 	require("nvoid.plugins.config.lualine.styles.nvoid")
 elseif config.ui.statusline == "vscode" then
-	require("nvoid.plugins.config.lualine.style.vscode")
+	require("nvoid.plugins.config.lualine.styles.vscode")
 elseif config.ui.statusline == "lunarvim" then
 	require("nvoid.plugins.config.lualine.lunarvim")
 elseif config.ui.statusline == "nvchad" then
