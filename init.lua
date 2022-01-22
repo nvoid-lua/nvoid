84a2f045f0d66106a4852d072ff2a42bf5b0bc12
ysf grg <ysfgrg2007@gmail.com>
2022-01-23 00:26:51 +0200

    init

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
--- pcall(require, "custom.nvoidrc")
+pcall(require, "custom.nvoidrc")
 
-require("nvoid")
-
--- for _, module in ipairs({ "nvoid" }) do
--- local ok = pcall(require, module)
--- if not ok then
--- return
--- end
--- end
+for _, module in ipairs({ "nvoid" }) do
+	local ok = pcall(require, module)
+	if not ok then
+		return
+	end
+end
