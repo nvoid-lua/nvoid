fed00c2a3f71ecda12ab3e12f4907b0a4d9baab2
ysf grg <ysfgrg2007@gmail.com>
2022-01-23 15:47:54 +0200

    new git icons

--- lua/nvoid/colors/highlights.lua
+++ lua/nvoid/colors/highlights.lua
@@ -114,17 +114,14 @@ fg_bg("NvimTreeWindowPicker", red, black2)
 -- Telescope
 fg_bg("TelescopeBorder", darker_black, darker_black)
 fg_bg("TelescopePromptBorder", black2, black2)
-
 fg_bg("TelescopePromptNormal", white, black2)
 fg_bg("TelescopePromptPrefix", red, black2)
-
-bg("TelescopeNormal", darker_black)
-
 fg_bg("TelescopePreviewTitle", black, green)
 fg_bg("TelescopePromptTitle", black, red)
 fg_bg("TelescopeResultsTitle", darker_black, darker_black)
-
+fg("TelescopeMatching", yellow)
 bg("TelescopeSelection", black2)
+bg("TelescopeNormal", darker_black)
 
 -- keybinds cheatsheet
 
