940a0444047f06a79441e1c63fbc15bb238508f0
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-02-16 14:58:39 +0200

    dashboard

--- lua/nvoid/plugins/config/dashboard.lua
+++ lua/nvoid/plugins/config/dashboard.lua
@@ -1,15 +1,15 @@
 local g = vim.g
 local fn = vim.fn
 g.dashboard_disable_at_vimenter = 0
+g.dashboard_default_executive = "telescope"
 g.dashboard_custom_header = {
-	"███╗   ██╗██╗   ██╗ ██████╗ ██╗██████╗ ",
+	"███╗   ██╗██╗   ██╗ ██████╗ ██╗██████╗",
 	"████╗  ██║██║   ██║██╔═══██╗██║██╔══██╗",
 	"██╔██╗ ██║██║   ██║██║   ██║██║██║  ██║",
 	"██║╚██╗██║╚██╗ ██╔╝██║   ██║██║██║  ██║",
 	"██║ ╚████║ ╚████╔╝ ╚██████╔╝██║██████╔╝",
-	"╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═╝╚═════╝ ",
+	"╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═╝╚═════╝",
 }
-g.dashboard_default_executive = "telescope"
 g.dashboard_custom_section = {
 	a = { description = { "  Find File        " }, command = ":Telescope find_files" },
 	b = { description = { "  New File         " }, command = ":ene!" },
@@ -21,9 +21,9 @@ g.dashboard_custom_section = {
 local nvoid_site = "爵nvoid.org"
 local plugins_count = fn.len(fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 local footer = {
-	"Nvoid loaded " .. plugins_count .. " plugins ",
+	"Nvoid loaded " .. plugins_count .. " plugins  ",
 	"",
 	nvoid_site,
 }
 local text = require("nvoid.core.text")
-g.dashboard_custom_footer = text.align_center({ width = 0 }, footer, 0.49)
+g.dashboard_custom_footer = text.align_center({ width = 0 }, footer, 0.50)
