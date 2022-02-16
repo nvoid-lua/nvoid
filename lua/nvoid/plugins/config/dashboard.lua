8b1c1863cc4bcd4f279eb33c9e0b3a54602ff441
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-02-16 14:17:12 +0200

    switch to dashboard

--- lua/nvoid/plugins/config/dashboard.lua
+++ lua/nvoid/plugins/config/dashboard.lua
@@ -1,5 +1,7 @@
-vim.g.dashboard_disable_at_vimenter = 0
-vim.g.dashboard_custom_header = {
+local g = vim.g
+local fn = vim.fn
+g.dashboard_disable_at_vimenter = 0
+g.dashboard_custom_header = {
 	"███╗   ██╗██╗   ██╗ ██████╗ ██╗██████╗ ",
 	"████╗  ██║██║   ██║██╔═══██╗██║██╔══██╗",
 	"██╔██╗ ██║██║   ██║██║   ██║██║██║  ██║",
@@ -7,21 +9,21 @@ vim.g.dashboard_custom_header = {
 	"██║ ╚████║ ╚████╔╝ ╚██████╔╝██║██████╔╝",
 	"╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═╝╚═════╝ ",
 }
-vim.g.dashboard_default_executive = "telescope"
-vim.g.dashboard_custom_section = {
-	a = { description = { "  Find File          " }, command = "Telescope find_files" },
-	b = { description = { "  New File           " }, command = ":ene!" },
-	c = { description = { "  Recent Projects    " }, command = "Telescope projects" },
-	d = { description = { "  Recently Used Files" }, command = "Telescope oldfiles" },
-	e = { description = { "  Find Word          " }, command = "Telescope live_grep" },
-	f = { description = { "  Configuration      " }, command = ":e ~/.config/nvim/lua/custom/nvoidrc.lua" },
+g.dashboard_default_executive = "telescope"
+g.dashboard_custom_section = {
+	a = { description = { "  Find File        " }, command = ":Telescope find_files" },
+	b = { description = { "  New File         " }, command = ":ene!" },
+	c = { description = { "  Recent Files     " }, command = ":Telescope oldfiles" },
+	d = { description = { "  Configuration    " }, command = ":e ~/.config/nvim/lua/custom/nvoidrc.lua" },
+	e = { description = { "  Load Last Session" }, command = ":SessionLoad" },
+	f = { description = { "  Quit Nvim        " }, command = ":q!" },
 }
-local nvoid_site = "nvoid.org"
-local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+local nvoid_site = "爵nvoid.org"
+local plugins_count = fn.len(fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 local footer = {
-	"Nvoid loaded " .. plugins_count .. " plugins  ",
+	"Nvoid loaded " .. plugins_count .. " plugins ",
 	"",
 	nvoid_site,
 }
 local text = require("nvoid.core.text")
-vim.g.dashboard_custom_footer = text.align_center({ width = 0 }, footer, 0.49) -- Use 0.49 as  counts for 2 characters
+g.dashboard_custom_footer = text.align_center({ width = 0 }, footer, 0.49)
