61983586e123055bed87faa6e5ef43640598f5e9
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-05-28 23:24:35 +0200

    updating formats

--- lua/nvoid/plugins/config/lsp/init.lua
+++ lua/nvoid/plugins/config/lsp/init.lua
@@ -1,6 +1,6 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-	return
+  return
 end
 require("nvoid.plugins.config.lsp.lsp-installer")
 require("nvoid.plugins.config.lsp.settings")
