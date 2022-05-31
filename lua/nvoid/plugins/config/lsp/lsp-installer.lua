2fd2e3d7fe6213b072fb4d2b4adb64e438848906
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-05-31 16:10:03 +0200

    fixing sumneko_lua server when adding others in the custom config

--- lua/nvoid/plugins/config/lsp/lsp-installer.lua
+++ lua/nvoid/plugins/config/lsp/lsp-installer.lua
@@ -30,23 +30,23 @@ local options = {
 lsp_installer.setup(options)
 
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
+-- local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+-- if not lspconfig_status_ok then
+  -- return
+-- end
 
-local opts = {}
+-- local opts = {}
 
-for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("nvoid.plugins.config.lsp.handlers").on_attach,
-    capabilities = require("nvoid.plugins.config.lsp.handlers").capabilities,
-  }
+-- for _, server in pairs(servers) do
+  -- opts = {
+    -- on_attach = require("nvoid.plugins.config.lsp.handlers").on_attach,
+    -- capabilities = require("nvoid.plugins.config.lsp.handlers").capabilities,
+  -- }
 
-  if server == "sumneko_lua" then
-    local sumneko_opts = require "nvoid.plugins.config.lsp.settengs.sumneko_lua"
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
+  -- if server == "sumneko_lua" then
+    -- local sumneko_opts = require "nvoid.plugins.config.lsp.settengs.sumneko_lua"
+    -- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  -- end
 
-  lspconfig[server].setup(opts)
-end
+  -- lspconfig[server].setup(opts)
+-- end
