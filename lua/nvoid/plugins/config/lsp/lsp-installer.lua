7a105347f3e3337070d089a2aca509620ab2bedc
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-06-14 14:26:17 +0200

    Cleaning

--- lua/nvoid/plugins/config/lsp/lsp-installer.lua
+++ lua/nvoid/plugins/config/lsp/lsp-installer.lua
@@ -28,25 +28,3 @@ local options = {
 }
 
 lsp_installer.setup(options)
-
-
--- local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
--- if not lspconfig_status_ok then
-  -- return
--- end
-
--- local opts = {}
-
--- for _, server in pairs(servers) do
-  -- opts = {
-    -- on_attach = require("nvoid.plugins.config.lsp.handlers").on_attach,
-    -- capabilities = require("nvoid.plugins.config.lsp.handlers").capabilities,
-  -- }
-
-  -- if server == "sumneko_lua" then
-    -- local sumneko_opts = require "nvoid.plugins.config.lsp.settengs.sumneko_lua"
-    -- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  -- end
-
-  -- lspconfig[server].setup(opts)
--- end
