7fc3983f88b8386ed825859941622dc1e13029ac
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-07-13 23:14:30 +0200

    fix

--- lua/nvoid/plugins/config/treesitter.lua
+++ lua/nvoid/plugins/config/treesitter.lua
@@ -2,47 +2,49 @@ local present, ts_config = pcall(require, "nvim-treesitter.configs")
 local config = require("nvoid.core.utils").load_config()
 
 if not present then
-	return
+  return
 end
 
-ts_config.setup({
-	use_languagetree = true,
-	on_config_done = nil,
-	ensure_installed = config.ts_add,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-		disable = { "latex" },
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = true,
-		config = {
-			typescript = "// %s",
-			css = "/* %s */",
-			scss = "/* %s */",
-			html = "<!-- %s -->",
-			svelte = "<!-- %s -->",
-			vue = "<!-- %s -->",
-			json = "",
-		},
-	},
-	matchup = {
-		enable = false,
-	},
-	indent = { enable = true, disable = { "yaml" } },
-	autotag = { enable = false },
-	textobjects = {
-		swap = {
-			enable = false,
-		},
-		select = {
-			enable = false,
-		},
-	},
-	rainbow = {
-		enable = false,
-		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-	},
-})
+ts_config.setup {
+  use_languagetree = true,
+  on_config_done = nil,
+  ensure_installed = config.ts_add,
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+    disable = { "latex" },
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+    config = {
+      typescript = "// %s",
+      css = "/* %s */",
+      scss = "/* %s */",
+      html = "<!-- %s -->",
+      svelte = "<!-- %s -->",
+      vue = "<!-- %s -->",
+      json = "",
+    },
+  },
+  matchup = {
+    enable = false,
+  },
+  indent = { enable = true, disable = { "yaml", "python" } },
+  autotag = { enable = false },
+  textobjects = {
+    swap = {
+      enable = false,
+    },
+    select = {
+      enable = false,
+    },
+  },
+  rainbow = {
+    enable = false,
+    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+  },
+}
