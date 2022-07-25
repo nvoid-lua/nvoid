d23dc3b220714612291c525b8860234c2a44ffe4
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-07-25 22:48:17 +0200

    fixing the diagnostics error

--- lua/nvoid/core/cmd.lua
+++ lua/nvoid/core/cmd.lua
@@ -1,10 +1,10 @@
 local cmd = vim.cmd
 -- Nvoid Stuff
 cmd [[ command! NvoidUpdater lua _UPDATER() ]]
+cmd [[ command! NvoidEditConfig e ~/.config/nvim/lua/custom/nvoidrc.lua ]]
 -- Lsp Stuff
 cmd [[ command! NvoidRename lua require("nvoid.ui").rename() ]]
 cmd [[ command! NvoidCodeActions lua require("nvoid.ui").code_actions() ]]
 cmd [[ command! NvoidRangeCodeActions lua require("nvoid.ui").range_code_actions() ]]
 cmd [[ command! NvoidFormat lua vim.lsp.buf.formatting() ]]
-cmd [[ command! NvoidDiagnostics lua vim.lsp.diagnostic.show_line_diagnostics() ]]
-cmd [[ command! NvoidEditConfig e ~/.config/nvim/lua/custom/nvoidrc.lua ]]
+cmd [[ command! NvoidDiagnostics lua vim.diagnostic.open_float(0, { show_header = false, severity_sort = true, scope = "line", }) ]]
