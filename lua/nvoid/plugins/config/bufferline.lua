0f6c551dc85d622a703d16327bd2ce9df3f2bee7
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-07-29 00:20:16 +0200

    Moving bufferline coloring

--- lua/nvoid/plugins/config/bufferline.lua
+++ lua/nvoid/plugins/config/bufferline.lua
@@ -3,7 +3,6 @@ if not present1 then
   return
 end
 
-local colors = require("base16").get()
 local icons = require("nvoid.ui.icons").bufferline
 
 bufferline.setup {
@@ -37,96 +36,5 @@ bufferline.setup {
     enforce_regular_tabs = false,
     always_show_bufferline = false,
     sort_by = "id",
-  },
-  highlights = {
-    background = {
-      guifg = colors.grey_fg,
-      guibg = colors.black2,
-    },
-
-    -- buffers
-    buffer_selected = {
-      guifg = colors.white,
-      guibg = colors.black,
-      gui = "bold",
-    },
-    buffer_visible = {
-      guifg = colors.light_grey,
-      guibg = colors.black2,
-    },
-
-    -- for diagnostics = "nvim_lsp"
-    error = {
-      guifg = colors.light_grey,
-      guibg = colors.black2,
-    },
-    error_diagnostic = {
-      guifg = colors.light_grey,
-      guibg = colors.black2,
-    },
-
-    -- close buttons
-    close_button = {
-      guifg = colors.light_grey,
-      guibg = colors.black2,
-    },
-    close_button_visible = {
-      guifg = colors.light_grey,
-      guibg = colors.black2,
-    },
-    close_button_selected = {
-      guifg = colors.red,
-      guibg = colors.black,
-    },
-    fill = {
-      guifg = colors.grey_fg,
-      guibg = colors.black2,
-    },
-    indicator_selected = {
-      guifg = colors.blue,
-      guibg = colors.black,
-    },
-
-    -- modified
-    modified = {
-      guifg = colors.red,
-      guibg = colors.black2,
-    },
-    modified_visible = {
-      guifg = colors.red,
-      guibg = colors.black2,
-    },
-    modified_selected = {
-      guifg = colors.green,
-      guibg = colors.black,
-    },
-
-    -- separators
-    separator = {
-      guifg = colors.black2,
-      guibg = colors.black2,
-    },
-    separator_visible = {
-      guifg = colors.black2,
-      guibg = colors.black2,
-    },
-    separator_selected = {
-      guifg = colors.black2,
-      guibg = colors.black2,
-    },
-
-    -- tabs
-    tab = {
-      guifg = colors.light_grey,
-      guibg = colors.one_bg3,
-    },
-    tab_selected = {
-      guifg = colors.black2,
-      guibg = colors.nord_blue,
-    },
-    tab_close = {
-      guifg = colors.red,
-      guibg = colors.black,
-    },
   },
 }
