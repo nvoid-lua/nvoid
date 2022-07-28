7dd327301df85e8ee13a792f6ed85f11b94efb8b
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-07-28 22:47:51 +0200

    Clean

--- lua/nvoid/core/cmd.lua
+++ lua/nvoid/core/cmd.lua
@@ -3,8 +3,6 @@ local cmd = vim.cmd
 cmd [[ command! NvoidUpdater lua _UPDATER() ]]
 cmd [[ command! NvoidEditConfig e ~/.config/nvim/lua/custom/nvoidrc.lua ]]
 -- Lsp Stuff
-cmd [[ command! NvoidRename lua require("nvoid.ui").rename() ]]
-cmd [[ command! NvoidCodeActions lua require("nvoid.ui").code_actions() ]]
-cmd [[ command! NvoidRangeCodeActions lua require("nvoid.ui").range_code_actions() ]]
+cmd [[ command! NvoidRename lua require("nvoid.ui.rename").open() ]]
 cmd [[ command! NvoidFormat lua vim.lsp.buf.formatting() ]]
 cmd [[ command! NvoidDiagnostics lua vim.diagnostic.open_float(0, { show_header = false, severity_sort = true, scope = "line", }) ]]
