f7a91b6e6c5a134c5a172e1e61607304c448f973
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-03 16:15:57 +0200

    Packer

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 pcall(require, "custom.nvoidrc")
+require("nvoid.core.packer").load()
+local modules = {
+  "nvoid.core",
+  "nvoid.core.map"
+}
 
-for _, module in ipairs { "nvoid" } do
+for _, module in ipairs(modules) do
   local ok = pcall(require, module)
   if not ok then
     return
