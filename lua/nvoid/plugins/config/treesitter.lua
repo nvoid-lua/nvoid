7775719aa24f1e16b9148e6c6a1dd5a77f5fb47e
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-07 22:20:59 +0200

    cleaning

--- lua/nvoid/plugins/config/treesitter.lua
+++ lua/nvoid/plugins/config/treesitter.lua
@@ -6,45 +6,14 @@ if not present then
 end
 
 ts_config.setup {
-  use_languagetree = true,
-  on_config_done = nil,
   ensure_installed = config.ts_add,
-  sync_install = false,
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false,
-    disable = { "latex" },
-  },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-    config = {
-      typescript = "// %s",
-      css = "/* %s */",
-      scss = "/* %s */",
-      html = "<!-- %s -->",
-      svelte = "<!-- %s -->",
-      vue = "<!-- %s -->",
-      c = "// %s",
-    },
+    use_languagetree = true,
   },
   matchup = {
-    enable = false,
+    enable = true,
   },
-  indent = { enable = true, disable = { "yaml", "python" } },
-  autotag = { enable = false },
-  textobjects = {
-    swap = {
-      enable = false,
-    },
-    select = {
-      enable = false,
-    },
-  },
-  rainbow = {
-    enable = false,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-  },
+  indent = { enable = true },
 }
