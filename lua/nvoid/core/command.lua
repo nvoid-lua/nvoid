fb1ba4d653da2efa8dc6b95d3388e402a43145f4
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-10 22:50:47 +0200

    cleaning

--- lua/nvoid/core/command.lua
+++ lua/nvoid/core/command.lua
@@ -2,34 +2,6 @@
 local cmd = vim.cmd
 local new_cmd = vim.api.nvim_create_user_command
 
--- Bufferline
-new_cmd("BufNext", function()
-  require("nvoid.core.utils").bufferlinenext()
-end, {})
-new_cmd("BufPrev", function()
-  require("nvoid.core.utils").bufferlineprev()
-end, {})
-new_cmd("BufClose", function()
-  vim.cmd [[bdelete %]]
-end, {})
-
-new_cmd("BufPick", function()
-  vim.g.bufpick_showNums = true
-  vim.cmd "redrawtabline"
-  vim.api.nvim_echo({ { "Enter Num ", "Question" } }, false, {})
-  local key = tonumber(vim.fn.nr2char(vim.fn.getchar()))
-  if key then
-    vim.cmd("b" .. vim.t.bufs[key])
-    vim.api.nvim_echo({ { "" } }, false, {})
-    vim.cmd "redraw"
-  else
-    vim.cmd "redraw"
-    print "bufpick cancelled, press a number key!"
-  end
-  vim.g.bufpick_showNums = false
-  vim.cmd "redrawtabline"
-end, {})
-
 -- Nvoid
 new_cmd("NvoidUpdater", function()
   _UPDATER()
@@ -39,9 +11,6 @@ new_cmd("NvoidEditConfig", function()
 end, {})
 
 -- Lsp
-new_cmd("NvoidRename", function()
-  require("nvoid.ui.rename").open()
-end, {})
 new_cmd("NvoidFormat", function()
   vim.lsp.buf.formatting()
 end, {})
