7dfad0e94a313dcef95fc7ea3b555a8e5ab4dbda
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-15 23:16:57 +0200

    Fixing lsp errors with neovim nightly

--- lua/nvoid/plugins/config/lsp/config.lua
+++ lua/nvoid/plugins/config/lsp/config.lua
@@ -78,13 +78,13 @@ M.AutoForamt = function()
   end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
+local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_cmp_ok then
   return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 
 return M
