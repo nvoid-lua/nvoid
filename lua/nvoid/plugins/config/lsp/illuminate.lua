68ba6f1954997c7c3c8bd6d755cdcd97aa8b7d1c
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-16 00:05:02 +0200

    Fixing lsp errors with neovim nightly

--- lua/nvoid/plugins/config/lsp/illuminate.lua
+++
@@ -1,8 +0,0 @@
-vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
-vim.api.nvim_set_keymap("n", "<a-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
-vim.api.nvim_set_keymap(
-  "n",
-  "<a-p>",
-  '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
-  { noremap = true }
-)
--- lua/nvoid/plugins/config/lsp/init.lua
+++ lua/nvoid/plugins/config/lsp/init.lua
@@ -4,6 +4,5 @@ require "nvoid.plugins.config.lsp.lsp-installer"
 require("nvoid.plugins.config.lsp.config").setup()
 require("nvoid.plugins.config.lsp.config").AutoForamt()
 require "nvoid.plugins.config.lsp.null-ls"
-require "nvoid.plugins.config.lsp.illuminate"
 
 return M
