7497f859de16e0be542cff405313332aa6a8f54a
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-19 16:52:49 +0200

    Formatting

--- lua/nvoid/plugins/config/lsp/null_ls.lua
+++ lua/nvoid/plugins/config/lsp/null_ls.lua
@@ -5,10 +5,11 @@ end
 local builtins = null_ls.builtins
 local sources = {
   builtins.formatting.stylua,
+  builtins.formatting.black.with({ extra_args = { "--fast" } }),
+  builtins.diagnostics.flake8,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
-}
-
+})
