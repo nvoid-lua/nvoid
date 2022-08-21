ba4b2b621bb53ba3881b23686f1c7aab593bd385
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-21 14:55:14 +0200

    Fix neovim v0.8

--- lua/nvoid/plugins/config/lsp/mason/utils.lua
+++ lua/nvoid/plugins/config/lsp/mason/utils.lua
@@ -12,7 +12,7 @@ M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 if config.lsp.document_highlight then
   M.setup_document_highlight = function(client, bufnr)
     local status_ok, highlight_supported = pcall(function()
-      return client.supports_method "textDocument/documentHighlight"
+      return client.supports_method("textDocument/documentHighlight")
     end)
     if not status_ok or not highlight_supported then
       return
@@ -34,23 +34,40 @@ if config.lsp.document_highlight then
       callback = vim.lsp.buf.clear_references,
     })
   end
-  M.on_attach = function(client, bufnr)
-    if client.name == "tsserver" or client.name == "sumneko_lua" then
-      client.resolved_capabilities.document_formatting = false
-    end
-    if client.resolved_capabilities.document_highlight then
-      vim.api.nvim_create_augroup("lsp_document_highlight", {})
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        group = "lsp_document_highlight",
-        buffer = 0,
-        callback = vim.lsp.buf.document_highlight,
-      })
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        group = "lsp_document_highlight",
-        buffer = 0,
-        callback = vim.lsp.buf.clear_references,
-      })
+  M.on_attach = function(client)
+    if vim.g.vim_version > 7 then
+      if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_create_augroup("lsp_document_highlight", {})
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          group = "lsp_document_highlight",
+          buffer = 0,
+          callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd("CursorMoved", {
+          group = "lsp_document_highlight",
+          buffer = 0,
+          callback = vim.lsp.buf.clear_references,
+        })
+      end
+    else
+      if client.resolved_capabilities.document_highlight then
+        if client.name == "tsserver" or client.name == "sumneko_lua" then
+          client.resolved_capabilities.document_formatting = false
+        end
+        vim.api.nvim_create_augroup("lsp_document_highlight", {})
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          group = "lsp_document_highlight",
+          buffer = 0,
+          callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd("CursorMoved", {
+          group = "lsp_document_highlight",
+          buffer = 0,
+          callback = vim.lsp.buf.clear_references,
+        })
+      end
     end
   end
 end
+
 return M
