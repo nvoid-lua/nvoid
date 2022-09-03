0f4b4e64976b56e6c76fd46adcb14ac4a9332949
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-09-03 15:25:14 +0200

    cleaning

--- lua/nvoid/core/utils.lua
+++ lua/nvoid/core/utils.lua
@@ -1,31 +1,21 @@
 local M = {}
+local merge_tb = vim.tbl_deep_extend
 
 M.update_nvoid = require("nvoid.core.updater.update")
 
 M.load_config = function()
-  local conf = require("nvoid.core.def-config")
-  local nvoidrcExists, change = pcall(require, "custom.nvoidrc")
-  if nvoidrcExists then
-    conf = vim.tbl_deep_extend("force", conf, change)
-  end
-  return conf
-end
+  local config = require("nvoid.core.def-config")
+  local nvoidrc_exists, nvoidrc = pcall(require, "custom.nvoidrc")
 
-M.override_req = function(name, default_req)
-  local override = require("nvoid.core.utils").load_config().plugins.default_plugin_config_replace[name]
-  local result = default_req
-
-  if override ~= nil then
-    result = override
+  if nvoidrc_exists then
+    if type(nvoidrc) == "table" then
+      config = merge_tb("force", config, nvoidrc) or {}
+    else
+      error("nvoidrc must return a table!")
+    end
   end
 
-  if string.match(result, "^%(") then
-    result = result:sub(2)
-    result = result:gsub("%)%.", "').", 1)
-    return "require('" .. result
-  end
-
-  return "require('" .. result .. "')"
+  return config
 end
 
 -- Updater
