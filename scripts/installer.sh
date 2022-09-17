7e6e690f30d95eb701237724a5eeccc5d087eabc
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-09-18 00:19:55 +0200

    adding some stuff for the installer

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -2,22 +2,45 @@
 
 ## Checking neovim
 pkg_nvim_installed() {
+  # if it's not install install it
+  read -p "Neovim is not installed! would you like to install it? (default no)  " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
   if command -v pacman &> /dev/null
   then
+    sudo pacman -Syu --needed neovim
+  elif command -v xbps-install &> /dev/null
+  then
+    sudo xbps-install -Syu neovim
+  elif command -v apt-get &> /dev/null
+  then
+    echo "Visit: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package" 
+    exit 0
+  elif command -v dnf &> /dev/null
+  then
+   sudo dnf install neovim
+  fi
+  else
+  if command -v pacman &> /dev/null
+  then
+    echo "### Neovim is not installed try:"
     echo "sudo pacman -Syu --needed neovim"
   elif command -v xbps-install &> /dev/null
   then
     echo "sudo xbps-install -Syu neovim"
   elif command -v apt-get &> /dev/null
   then
-    echo "Visit: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package" 
+    echo "### Neovim is not installed try:"
+    echo "Visit: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package"
   elif command -v dnf &> /dev/null
   then
+   echo "### Neovim is not installed try:"
    echo "sudo dnf install neovim"
+  fi
   fi
 }
 warnnvim() {
-  echo "### Neovim is not installed try:"
   echo $(pkg_nvim_installed)
   exit
 }
@@ -29,7 +52,6 @@ function check_neovim_min_version() {
     exit 1
   fi
 }
-check_neovim_min_version
 
 which nvim >/dev/null && echo "Neovim 0.7.0 is installed" || warnnvim
 
@@ -202,30 +224,70 @@ packer() {
     nvim --headless +'autocmd User PackerComplete sleep 10m | qall' +PackerSync
 }
 
-### Config
-nvoidrc() {
-  cd ~/.config/nvim/lua/
-  cp -r ../example ./custom 
-}
-
-config() {
-  read -p "Do you want to Create an nvoidrc ? (default no)  " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    nvoidrc
-  fi
-}
-
-echo "Hello, $(whoami)!"
-install_deps
-copy_old_config
-clone_repo
-clone_packer
-
-config
-
-echo "Preparing Packer setup"
-packer
-echo -e "\nCompile Complete"
-nvim
+if [ "$1" == "-h" ] ; then
+  # help
+  echo "Welcome to the NVoid Installer!"
+  echo "options:"
+  echo "    -y: Install Nvoid with all the dependencies needed"
+  echo "    -n: Install Nvoid without any user choice"
+  exit 0
+elif [ "$1" == "-y" ]; then
+ # Yes to all opts
+  echo "Hello, $(whoami)!"
+  check_neovim_min_version
+ # Npm
+  nodey() {
+    echo "### nodejs or npm are not installed"
+    echo "skipping..."
+  }
+  which node npm >/dev/null && echo "Node and npm are installed" || nodey
+  sudo npm i -g neovim --force
+  sudo npm i -g tree-sitter-cli --force
+ # python
+  pythony() {
+    echo "### pip is not installed"
+    echo "skipping..."
+  }
+  which node npm >/dev/null && echo "pip is installed" || pythony
+  pip install --force-reinstall pynvim
+ # Cargo
+  cargoy() {
+    echo "### cargo is not installed"
+    echo "skipping..."
+  }
+  which node npm >/dev/null && echo "cargo is installed" || cargoy
+  cargo install stylua fd-find
+  cargo install stylua stylua
+  sudo cp -r ~/.cargo/bin/fd /bin/
+  sudo cp -r ~/.cargo/bin/stylua /bin/
+  copy_old_config
+  clone_repo
+  clone_packer
+  echo "Preparing Packer setup"
+  packer
+  echo -e "\nCompile Complete"
+  nvim
+elif [ "$1" == "-n" ]; then
+ # No to all opts
+  echo "Hello, $(whoami)!"
+  check_neovim_min_version
+  copy_old_config
+  clone_repo
+  clone_packer
+  echo "Preparing Packer setup"
+  packer
+  echo -e "\nCompile Complete"
+  nvim
+else
+ # Normal
+  echo "Hello, $(whoami)!"
+  check_neovim_min_version
+  install_deps
+  copy_old_config
+  clone_repo
+  clone_packer
+  echo "Preparing Packer setup"
+  packer
+  echo -e "\nCompile Complete"
+  nvim
+fi
