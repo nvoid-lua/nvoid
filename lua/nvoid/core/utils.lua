c345130c82487e677e8c989a1f650e9b367f37db
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-09-18 22:29:44 +0200

    tmp

--- lua/nvoid/core/utils.lua
+++ lua/nvoid/core/utils.lua
@@ -1,8 +1,5 @@
 local M = {}
 local merge_tb = vim.tbl_deep_extend
-
-M.update_nvoid = require("nvoid.core.updater.update")
-
 M.load_config = function()
   local config = require("nvoid.core.def-config")
   local nvoidrc_exists, nvoidrc = pcall(require, "custom.nvoidrc")
@@ -17,30 +14,4 @@ M.load_config = function()
 
   return config
 end
-
--- Updater
-M.echo = function(opts)
-  if opts == nil or type(opts) ~= "table" then
-    return
-  end
-  vim.api.nvim_echo(opts, false, {})
-end
-M.clear_last_echo = function()
-  -- wrap this with inputsave and inputrestore just in case
-  vim.fn.inputsave()
-  vim.api.nvim_feedkeys(":", "nx", true)
-  vim.fn.inputrestore()
-end
-
-M.cmd = function(cmd, print_error)
-  local result = vim.fn.system(cmd)
-  if vim.api.nvim_get_vvar("shell_error") ~= 0 then
-    if print_error then
-      vim.api.nvim_err_writeln("Error running command:\n" .. cmd .. "\nError message:\n" .. result)
-    end
-    return nil
-  end
-  return result
-end
-
 return M
