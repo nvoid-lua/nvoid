d8b813ca407572f8298ebb8a047e190bab7ef1bc
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-10-11 21:25:28 +0200

    Updating the config

--- lua/nvoid/plugins/config/treesitter.lua
+++ lua/nvoid/plugins/config/treesitter.lua
@@ -5,70 +5,16 @@ M.config = function()
   nvoid.builtin.treesitter = {
     on_config_done = nil,
     ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {},
+    auto_install = true,
     matchup = {
-      enable = false, -- mandatory, false will disable the whole extension
-      -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+      enable = true, -- mandatory, false will disable the whole extension
     },
     highlight = {
       enable = true, -- false will disable the whole extension
-      additional_vim_regex_highlighting = false,
-      disable = { "latex" },
+      use_languagetree = true,
     },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-      config = {
-        -- Languages that have a single comment style
-        typescript = "// %s",
-        css = "/* %s */",
-        scss = "/* %s */",
-        html = "<!-- %s -->",
-        svelte = "<!-- %s -->",
-        vue = "<!-- %s -->",
-        json = "",
-      },
-    },
-    indent = { enable = true, disable = { "yaml", "python" } },
-    autotag = { enable = false },
-    textobjects = {
-      swap = {
-        enable = false,
-        -- swap_next = textobj_swap_keymaps,
-      },
-      -- move = textobj_move_keymaps,
-      select = {
-        enable = false,
-        -- keymaps = textobj_sel_keymaps,
-      },
-    },
-    textsubjects = {
-      enable = false,
-      keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-    },
-    playground = {
-      enable = false,
-      disable = {},
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = "o",
-        toggle_hl_groups = "i",
-        toggle_injected_languages = "t",
-        toggle_anonymous_nodes = "a",
-        toggle_language_display = "I",
-        focus_language = "f",
-        unfocus_language = "F",
-        update = "R",
-        goto_node = "<cr>",
-        show_help = "?",
-      },
-    },
-    rainbow = {
-      enable = false,
-      extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-      max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-    },
+    indent = { enable = true, },
+    ignore_install = {},
   }
 end
 
