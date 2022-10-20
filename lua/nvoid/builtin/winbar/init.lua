dbfbc812fc30ea64dd1885db23918e8a6e0176d6
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-10-20 19:11:52 +0200

    cmp config

--- lua/nvoid/builtin/winbar/init.lua
+++ lua/nvoid/builtin/winbar/init.lua
@@ -45,7 +45,7 @@ M.get_filename = function()
       file_icon_color = ""
     end
 
-    local navic_text = vim.api.nvim_get_hl_by_name("NavicText", true)
+    local navic_text = vim.api.nvim_get_hl_by_name("NavicSeparator", true)
     vim.api.nvim_set_hl(0, "Winbar", { fg = navic_text.foreground })
 
     return " " .. "%#" .. hl_group .. "#" .. file_icon .. "%*" .. " " .. "%#Winbar#" .. filename .. "%*"
