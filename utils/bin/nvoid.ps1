beb852e53d0d02b0ee6a72d60f533dd731006d84
Sarvex <sarvex@jatasra.com>
2023-04-05 23:24:47 +0530

    updated configuration

--- utils/bin/nvoid.ps1
+++ utils/bin/nvoid.ps1
@@ -6,8 +6,8 @@ $env:XDG_CONFIG_HOME = $env:XDG_CONFIG_HOME ?? $env:LOCALAPPDATA
 $env:XDG_CACHE_HOME = $env:XDG_CACHE_HOME ?? $env:TEMP
 
 $env:NVOID_RUNTIME_DIR = $env:NVOID_RUNTIME_DIR ?? "$env:XDG_DATA_HOME\nvoid"
-$env:NVOID_CONFIG_DIR = $env:NVOID_CONFIG_DIR ?? "$env:XDG_CONFIG_HOME\noid"
-$env:NVOID_CACHE_DIR = $env:NVOID_CACHE_DIR ?? "$env:XDG_CACHE_HOME\noid"
-$env:NVOID_BASE_DIR = $env:NVOID_BASE_DIR ?? "$env:NVOID_RUNTIME_DIR\noid"
+$env:NVOID_CONFIG_DIR = $env:NVOID_CONFIG_DIR ?? "$env:XDG_CONFIG_HOME\nvoid"
+$env:NVOID_CACHE_DIR = $env:NVOID_CACHE_DIR ?? "$env:XDG_CACHE_HOME\nvoid"
+$env:NVOID_BASE_DIR = $env:NVOID_BASE_DIR ?? "$env:NVOID_RUNTIME_DIR\nvoid"
 
 nvim -u "$env:NVOID_BASE_DIR\init.lua" @args
