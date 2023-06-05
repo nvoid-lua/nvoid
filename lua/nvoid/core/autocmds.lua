09cfc4aecd33a6e86c128c0f62777a761f13c6d0
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-06-06 00:44:24 +0300

    hide the statusline in alpha

--- lua/nvoid/core/autocmds.lua
+++ lua/nvoid/core/autocmds.lua
@@ -98,25 +98,19 @@ function M.load_defaults()
       },
     },
     {
-      "ColorScheme",
+      "FileType",
       {
-        group = "_nvoid_colorscheme",
-        callback = function()
-          -- if nvoid.builtin.breadcrumbs.active then
-          --   require("nvoid.core.breadcrumbs").get_winbar()
-          -- end
-          local statusline_hl = vim.api.nvim_get_hl_by_name("StatusLine", true)
-          local cursorline_hl = vim.api.nvim_get_hl_by_name("CursorLine", true)
-          local normal_hl = vim.api.nvim_get_hl_by_name("Normal", true)
-          vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-          vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
-          vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
-          vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
-          vim.api.nvim_set_hl(0, "SLCopilot", { fg = "#6CC644", bg = statusline_hl.background })
-          vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#E8AB53", bg = cursorline_hl.background })
-          vim.api.nvim_set_hl(0, "SLBranchName", { fg = normal_hl.foreground, bg = cursorline_hl.background })
-          vim.api.nvim_set_hl(0, "SLSeparator", { fg = cursorline_hl.background, bg = statusline_hl.background })
-        end,
+        group = "_dashboard_settings",
+        pattern = "alpha",
+        command = "set showtabline=0 | autocmd BufLeave <buffer> set showtabline=" .. vim.opt.showtabline._value,
+      },
+    },
+    {
+      "FileType",
+      {
+        group = "_dashboard_settings",
+        pattern = "alpha",
+        command = "set laststatus=0 | autocmd BufUnload <buffer> set laststatus=" .. vim.opt.laststatus._value,
       },
     },
     { -- taken from AstroNvim
