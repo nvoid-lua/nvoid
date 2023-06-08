9c3e9fcdb473af0304e03cda18b9ec8b51c334f4
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-06-08 18:09:08 +0300

    lazy loading gitsigns

--- lua/nvoid/plugins/init.lua
+++ lua/nvoid/plugins/init.lua
@@ -158,11 +158,27 @@ local core_plugins = {
   },
   {
     "lewis6991/gitsigns.nvim",
+    ft = { "gitcommit", "diff" },
+    init = function()
+      -- load gitsigns only when a git file is opened
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+            vim.schedule(function()
+              require("lazy").load { plugins = { "gitsigns.nvim" } }
+            end)
+          end
+        end,
+      })
+    end,
     config = function()
       require("nvoid.plugins.config.gitsigns").setup()
     end,
-    event = "User FileOpened",
-    cmd = "Gitsigns",
+    -- event = "User FileOpened",
+    -- cmd = "Gitsigns",
     enabled = nvoid.builtin.gitsigns.active,
   },
 
