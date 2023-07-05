572ec8fdccd96441adbcdbac90b41a1023a245f0
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-05 20:30:52 +0300

    winbar config

--- lua/nvoid/ui/winbar/functions.lua
+++ lua/nvoid/ui/winbar/functions.lua
@@ -1,37 +1,11 @@
 local M = {}
 
--- vim.cmd [[
---   function Test()
---     %SnipRun
---     call feedkeys("\<esc>`.")
---   endfunction
-
---   function TestI()
---     let b:caret = winsaveview()
---     %SnipRun
---     call winrestview(b:caret)
---   endfunction
--- ]]
-
 function M.sniprun_enable()
-  -- vim.cmd [[
-  --   %SnipRun
-
-  --   augroup _sniprun
-  --    autocmd!
-  --    autocmd TextChanged * call Test()
-  --    autocmd TextChangedI * call TestI()
-  --   augroup end
-  -- ]]
   vim.notify "Enabled SnipRun"
 end
 
 function M.disable_sniprun()
   M.remove_augroup "_sniprun"
-  -- vim.cmd [[
-  --   SnipClose
-  --   SnipTerminate
-  --   ]]
   vim.notify "Disabled SnipRun"
 end
 
@@ -48,8 +22,6 @@ function M.remove_augroup(name)
     vim.cmd("au! " .. name)
   end
 end
-
--- vim.cmd [[ command! SnipRunToggle execute 'lua require("user.functions").toggle_sniprun()' ]]
 
 -- get length of current word
 function M.get_word_length()
