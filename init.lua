b140436bf8b2cf858ab7617ae5e9adef0c33bc21
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-14 16:40:13 +0300

    making nvoid fast

--- init.lua
+++ init.lua
@@ -16,6 +16,22 @@ vim.g.theme = nvoid.ui.colorscheme
 vim.g.transparency = nvoid.ui.transparency
 
 if not vim.loop.fs_stat then
+  local echo = function(str)
+    vim.cmd "redraw"
+    vim.api.nvim_echo({ { str, "Bold" } }, true, {})
+  end
+
+  local function shell_call(args)
+    local output = vim.fn.system(args)
+    assert(vim.v.shell_error == 0, "External call failed with error code: " .. vim.v.shell_error .. "\n" .. output)
+  end
+  local lazy_path = vim.fn.stdpath "data" .. "/site/pack/lazy/opt/base16"
+
+  echo "  compiling base16 theme to bytecode ..."
+
+  local base16_repo = "https://github.com/nvoid-lua/base16"
+  shell_call { "git", "clone", "--depth", "1", base16_repo, lazy_path }
+  vim.opt.rtp:prepend(lazy_path)
   require("base16").compile()
 end
 
