19987efe3f736c76c9636d9133b74ff96c0188ee
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-14 16:49:15 +0300

    making nvoid fast

--- lua/nvoid/plugins/init.lua
+++ lua/nvoid/plugins/init.lua
@@ -1,10 +1,3 @@
-local cmp_events = function()
-  if nvoid.builtin.cmp.cmdline.enable then
-    return { "InsertEnter", "CmdlineEnter" }
-  else
-    return { "InsertEnter" }
-  end
-end
 local core_plugins = {
   {
     "neovim/nvim-lspconfig",
@@ -79,7 +72,13 @@ local core_plugins = {
         require("nvoid.plugins.config.cmp").setup()
       end
     end,
-    event = cmp_events(),
+    event = function()
+      if nvoid.builtin.cmp.cmdline.enable then
+        return { "InsertEnter", "CmdlineEnter" }
+      else
+        return { "InsertEnter" }
+      end
+    end,
     dependencies = {
       "cmp-nvim-lsp",
       "cmp_luasnip",
