09c7a170685e126d70c65cd088ff81387aa1b234
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-14 17:01:52 +0300

    making nvoid fast

--- lua/nvoid/ui/init.lua
+++ lua/nvoid/ui/init.lua
@@ -10,3 +10,17 @@ vim.schedule_wrap(require("nvoid.ui.terminal").init())
 if nvoid.ui.winbar then
   require "nvoid.ui.winbar"
 end
+
+dofile(vim.g.base16_cache .. "defaults")
+dofile(vim.g.base16_cache .. "cmp")
+dofile(vim.g.base16_cache .. "lsp")
+dofile(vim.g.base16_cache .. "devicons")
+dofile(vim.g.base16_cache .. "blankline")
+dofile(vim.g.base16_cache .. "syntax")
+dofile(vim.g.base16_cache .. "git")
+dofile(vim.g.base16_cache .. "nvimtree")
+dofile(vim.g.base16_cache .. "telescope")
+dofile(vim.g.base16_cache .. "whichkey")
+dofile(vim.g.base16_cache .. "statusline")
+dofile(vim.g.base16_cache .. "bufferline")
+dofile(vim.g.base16_cache .. "navic")
