709b4e0e2440d89218fc7c704e76a5d7f31af7e3
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-16 00:13:46 +0300

    Formatting

--- lua/nvoid/ui/statusline/modules.lua
+++ lua/nvoid/ui/statusline/modules.lua
@@ -145,7 +145,7 @@ end
 M.get_lsp = function()
   local buf_clients = vim.lsp.get_active_clients { bufnr = 0 }
   if #buf_clients == 0 then
-    return "LSP Inactive"
+    return ""
   end
 
   local buf_ft = vim.bo.filetype
